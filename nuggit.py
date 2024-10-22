289ec6792da219cc5e4d5766094bc90ab3ccad23
Veronica Valeros <vero.valeros@gmail.com>
2024-10-22 22:45:26 +0200

    Use rich to visualise repo info

--- nuggit.py
+++ nuggit.py
@@ -4,6 +4,9 @@ import sys
 import os
 from dotenv import load_dotenv
 from util.github import validate_repo_url
+from util.github import get_repo_info
+from rich.console import Console
+from rich.table import Table
 
 
 def setup_logging(log_file, log_level):
@@ -74,7 +77,27 @@ def main():
 
     logging.info(f"Starting analysis of repository: {args.repo}")
 
+    repo_info = get_repo_info(owner, repository, github_token)
+    repo_topics = ', '.join(repo_info["Topics"])
 
+    console = Console()
+
+    table = Table(title="Nuggit Repository Information", row_styles=["reverse",""])
+
+    # Set max_width for columns
+    table.add_column(repo_info["Tool"], style="cyan", no_wrap=True, width=15)
+    table.add_column(repo_info["URL"], style="magenta", max_width=60, overflow="fold")
+
+
+    table.add_row("About", repo_info["About"])
+    table.add_row("Topics", repo_topics)
+    table.add_row("License", repo_info["License"])
+    table.add_row("Latest Release", repo_info["Latest Release"])
+    table.add_row("Stars", str(repo_info["Stars"]))
+    table.add_row("Forks", str(repo_info["Forks"]))
+    table.add_row("Issues", str(repo_info["Issues"]))
+
+    console.print(table)
 if __name__ == "__main__":
     main()
 
